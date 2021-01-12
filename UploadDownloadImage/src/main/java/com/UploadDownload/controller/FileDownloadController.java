2261ccab0eaf93723c9b10155c1028f0a4a7d2c4
arjun <arjunyadav102030@gmail.com>
2021-01-12 17:39:44 +0530

    first commit

--- UploadDownloadImage/src/main/java/com/UploadDownload/controller/FileDownloadController.java
+++ UploadDownloadImage/src/main/java/com/UploadDownload/controller/FileDownloadController.java
@@ -1,6 +1,10 @@
 package com.UploadDownload.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,12 +13,16 @@ import com.UploadDownload.service.DatabaseFileService;
 
 public class FileDownloadController {
 	@Autowired
-	DatabaseFileService databaseFileService;
+	DatabaseFileService fileStorageService;
 	
 	@GetMapping("/downloadFile/{fileId}")
-	public DatabaseFile downloadFile(@PathVariable Integer fileId) {
-		DatabaseFile databaseFile = databaseFileService.getFile(fileId).get();
-		return databaseFile;
-	}
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Integer fileId) {
+        DatabaseFile databaseFile = fileStorageService.getFile(fileId).get();
+        
+        return ResponseEntity.ok()
+            .contentType(MediaType.parseMediaType(databaseFile.getFileType()))
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + databaseFile.getFileName())
+            .body(new ByteArrayResource(databaseFile.getData()));
+    }
 
 }
